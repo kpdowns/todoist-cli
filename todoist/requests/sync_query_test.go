9f5ce4a97fd1dfe394f91962c8245ce8f031070a
Kevin Downs <kevindwns@gmail.com>
2020-04-11 05:18:17 +0200

    Add test to ensure query string is correctly created for sync requests

---
+++ todoist/requests/sync_query_test.go
@@ -0,0 +1,18 @@
+package requests
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWhenConstructingSyncQueryThenTheQueryStringIsCorrectlyFormed(t *testing.T) {
+	resourceTypes := ResourceTypes{"all"}
+	syncQuery := NewSyncQuery("token", "sync_token", resourceTypes)
+
+	expectedString := fmt.Sprintf(`token=token&sync_token=sync_token&resource_types=%s`, resourceTypes.ToString())
+	actualString := syncQuery.ToQueryString()
+
+	if expectedString != actualString {
+		t.Errorf("Expected '%s', but received '%s'", expectedString, actualString)
+	}
+}

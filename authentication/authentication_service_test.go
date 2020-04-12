43928e3d4cddc2aeaea4d1cb2e24835b7612813a
Kevin Downs <kevindwns@gmail.com>
2020-04-12 23:41:42 +0200

    Refactor tests and remove redundancy

--- authentication/authentication_service_test.go
+++ authentication/authentication_service_test.go
@@ -1,7 +1,6 @@
 package authentication
 
 import (
-	"errors"
 	"fmt"
 	"testing"
 
@@ -10,228 +9,181 @@ import (
 	"github.com/kpdowns/todoist-cli/config"
 	"github.com/kpdowns/todoist-cli/mocks"
 	"github.com/kpdowns/todoist-cli/todoist/responses"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestGeneratedOauthURLHasTheExpectedFormatFromValuesInConfiguration(t *testing.T) {
-	guid := guid.NewString()
-	mockAPI := &mocks.MockAPI{}
-	mockRepository := &mocks.MockAuthenticationRepository{
-		AccessToken: "test",
-	}
-	mockServer := &mocks.MockAuthenticationServer{}
-	configuration := &config.TodoistCliConfiguration{
-		Client: config.ClientConfiguration{
-			TodoistURL:          "url",
-			ClientID:            "clientId",
-			RequiredPermissions: "permissions",
-		},
-	}
+func TestGeneratingOauthURL(t *testing.T) {
 
-	service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
+	t.Run("When generating an Oauth URL, the URL has the expected format", func(t *testing.T) {
 
-	expectedURL := fmt.Sprintf("%s/oauth/authorize?client_id=%s&scope=%s&state=%s",
-		configuration.Client.TodoistURL,
-		configuration.Client.ClientID,
-		configuration.Client.RequiredPermissions,
-		guid)
-	generatedOathURL := service.GetOauthURL(guid)
+		guid := guid.NewString()
+		mockAPI := &mocks.MockAPI{}
+		mockRepository := &mocks.MockAuthenticationRepository{
+			AccessToken: "test",
+		}
+		mockServer := &mocks.MockAuthenticationServer{}
+		configuration := &config.TodoistCliConfiguration{
+			Client: config.ClientConfiguration{
+				TodoistURL:          "url",
+				ClientID:            "clientId",
+				RequiredPermissions: "permissions",
+			},
+		}
 
-	if expectedURL != generatedOathURL {
-		t.Errorf("The generated Oauth URL was not in the correct format")
-	}
+		service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
+
+		expectedURL := fmt.Sprintf("%s/oauth/authorize?client_id=%s&scope=%s&state=%s",
+			configuration.Client.TodoistURL,
+			configuration.Client.ClientID,
+			configuration.Client.RequiredPermissions,
+			guid)
+		generatedOathURL := service.GetOauthURL(guid)
+
+		assert.Equal(t, expectedURL, generatedOathURL)
+
+	})
+
 }
 
-func TestIfAccessTokenIsSetThenIsAlreadyAuthenticated(t *testing.T) {
-	mockAPI := &mocks.MockAPI{}
-	mockRepository := &mocks.MockAuthenticationRepository{
-		AccessToken: "test",
-	}
-	mockServer := &mocks.MockAuthenticationServer{}
-	configuration := &config.TodoistCliConfiguration{
-		Client: config.ClientConfiguration{
-			TodoistURL:          "url",
-			ClientID:            "clientId",
-			RequiredPermissions: "permissions",
-		},
-	}
+func TestAccessTokenBeingSetDeterminesWhetherClientIsAuthenticated(t *testing.T) {
 
-	service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
+	t.Run("When the authentication repository returns an access token, then the client is authenticated", func(t *testing.T) {
 
-	isAuthenticated, _ := service.IsAuthenticated()
-	if !isAuthenticated {
-		t.Error("If the access token is set, then the todoist-cli should be authenticated")
-	}
+		mockAPI := &mocks.MockAPI{}
+		mockRepository := &mocks.MockAuthenticationRepository{
+			AccessToken: "test",
+		}
+		mockServer := &mocks.MockAuthenticationServer{}
+		configuration := &config.TodoistCliConfiguration{
+			Client: config.ClientConfiguration{
+				TodoistURL:          "url",
+				ClientID:            "clientId",
+				RequiredPermissions: "permissions",
+			},
+		}
+
+		service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
+
+		isAuthenticated, _ := service.IsAuthenticated()
+		assert.True(t, isAuthenticated)
+
+	})
+
+	t.Run("When the authentication repository does not return an access token, then the client is not authenticated", func(t *testing.T) {
+
+		mockAPI := &mocks.MockAPI{}
+		mockRepository := &mocks.MockAuthenticationRepository{
+			AccessToken: "",
+		}
+		mockServer := &mocks.MockAuthenticationServer{}
+		configuration := &config.TodoistCliConfiguration{
+			Client: config.ClientConfiguration{
+				TodoistURL:          "url",
+				ClientID:            "clientId",
+				RequiredPermissions: "permissions",
+			},
+		}
+
+		service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
+
+		isAuthenticated, _ := service.IsAuthenticated()
+		assert.False(t, isAuthenticated)
+
+	})
+
 }
 
-func TestIfAccessTokenIsNotSetThenTheTodoistCliIsNotAuthenticated(t *testing.T) {
-	mockAPI := &mocks.MockAPI{}
-	mockRepository := &mocks.MockAuthenticationRepository{
-		AccessToken: "",
-	}
-	mockServer := &mocks.MockAuthenticationServer{}
-	configuration := &config.TodoistCliConfiguration{
-		Client: config.ClientConfiguration{
-			TodoistURL:          "url",
-			ClientID:            "clientId",
-			RequiredPermissions: "permissions",
-		},
-	}
+func TestSigningIn(t *testing.T) {
 
-	service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
+	t.Run("When code is set to an empty string, then an error is returned", func(t *testing.T) {
 
-	isAuthenticated, _ := service.IsAuthenticated()
-	if isAuthenticated {
-		t.Error("If the access token is not set, then the todoist-cli should not be authenticated")
-	}
+		mockAPI := &mocks.MockAPI{}
+		mockRepository := &mocks.MockAuthenticationRepository{
+			AccessToken: "",
+		}
+		mockServer := &mocks.MockAuthenticationServer{}
+		configuration := &config.TodoistCliConfiguration{
+			Client: config.ClientConfiguration{
+				TodoistURL:          "url",
+				ClientID:            "clientId",
+				RequiredPermissions: "permissions",
+			},
+		}
+
+		service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
+
+		err := service.SignIn("")
+		assert.NotNil(t, err)
+		assert.Equal(t, errorNoCodeAvailableToSignInWith, err.Error())
+
+	})
+
+	t.Run("When code is not set to an empty string and no errors are received, then the access token is persisted and the client is authenticated", func(t *testing.T) {
+
+		mockAPI := &mocks.MockAPI{
+			GetAccessTokenFunction: func(code string) (*responses.AccessToken, error) {
+				return &responses.AccessToken{AccessToken: "access-token"}, nil
+			},
+		}
+		mockServer := &mocks.MockAuthenticationServer{}
+		mockRepository := &mocks.MockAuthenticationRepository{
+			AccessToken: "",
+		}
+		configuration := &config.TodoistCliConfiguration{}
+
+		service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
+
+		err := service.SignIn("test")
+		assert.Nil(t, err)
+		assert.Equal(t, "access-token", mockRepository.AccessToken)
+
+	})
+
+	t.Run("When no valid response is returned from Todoist, then an error is returned", func(t *testing.T) {
+
+		mockAPI := &mocks.MockAPI{
+			GetAccessTokenFunction: func(code string) (*responses.AccessToken, error) {
+				return &responses.AccessToken{AccessToken: ""}, nil
+			},
+		}
+		mockRepository := &mocks.MockAuthenticationRepository{
+			AccessToken: "access-token",
+		}
+		mockServer := &mocks.MockAuthenticationServer{
+			AuthenticationResponseToReturn: types.AuthenticationResponse{},
+		}
+		configuration := &config.TodoistCliConfiguration{}
+
+		service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
+
+		err := service.SignIn("code")
+		assert.NotNil(t, err)
+
+	})
+
 }
 
-func TestGivenCodeIsNotSetWhenAttemptingToSignInThenErrorOccurs(t *testing.T) {
-	mockAPI := &mocks.MockAPI{}
-	mockRepository := &mocks.MockAuthenticationRepository{
-		AccessToken: "",
-	}
-	mockServer := &mocks.MockAuthenticationServer{}
-	configuration := &config.TodoistCliConfiguration{
-		Client: config.ClientConfiguration{
-			TodoistURL:          "url",
-			ClientID:            "clientId",
-			RequiredPermissions: "permissions",
-		},
-	}
+func TestSigningOut(t *testing.T) {
 
-	service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
+	t.Run("When signing out and no errors are received, then no errors are returned and the client is no longer authenticated", func(t *testing.T) {
 
-	err := service.SignIn("")
-	if err == nil {
-		t.Errorf("Expected error to be received because code was set to empty string")
-	}
+		mockAPI := &mocks.MockAPI{
+			RevokeAccessTokenFunction: func(accessToken string) error {
+				return nil
+			},
+		}
+		mockServer := &mocks.MockAuthenticationServer{}
+		mockRepository := &mocks.MockAuthenticationRepository{
+			AccessToken: "access-token",
+		}
+		configuration := &config.TodoistCliConfiguration{}
 
-	if err != nil && err.Error() != errorNoCodeAvailableToSignInWith {
-		t.Errorf("Expected '%s', but got '%s'", errorNoCodeAvailableToSignInWith, err.Error())
-	}
-}
+		service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
 
-func TestIfCodeIsSetWhenAttemptingToSignInAndApiReturnsNoErrorsThenNoErrorsAreReturnedAndAccessTokenIsSaved(t *testing.T) {
-	mockAPI := &mocks.MockAPI{
-		GetAccessTokenFunction: func(code string) (*responses.AccessToken, error) {
-			return &responses.AccessToken{AccessToken: "access-token"}, nil
-		},
-	}
-	mockServer := &mocks.MockAuthenticationServer{}
-	mockRepository := &mocks.MockAuthenticationRepository{
-		AccessToken: "",
-	}
-	configuration := &config.TodoistCliConfiguration{}
+		err := service.SignOut()
+		assert.Nil(t, err)
+		assert.Equal(t, "", mockRepository.AccessToken)
 
-	service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
+	})
 
-	err := service.SignIn("test")
-	if err != nil {
-		t.Errorf("Expected no error to be returned because the mock mockAPI returned no errors")
-	}
-
-	isAuthenticated, err := service.IsAuthenticated()
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-
-	if !isAuthenticated {
-		t.Errorf("Expected to be authenticated")
-	}
-}
-
-func TestWhenSigningOutAndNoErrorsAreRecievedFromTheApiThenTheAccessTokenIsRemoved(t *testing.T) {
-	mockAPI := &mocks.MockAPI{
-		RevokeAccessTokenFunction: func(accessToken string) error {
-			return nil
-		},
-	}
-	mockServer := &mocks.MockAuthenticationServer{}
-	mockRepository := &mocks.MockAuthenticationRepository{
-		AccessToken: "access-token",
-	}
-	configuration := &config.TodoistCliConfiguration{}
-
-	service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
-
-	isAuthenticated, err := service.IsAuthenticated()
-	if err != nil || !isAuthenticated {
-		t.Errorf("Expected to be authenticated because we haven't yet signed out")
-	}
-
-	err = service.SignOut()
-	if err != nil {
-		t.Errorf("Expected no errors, but received '%s'", err.Error())
-	}
-
-	isAuthenticated, err = service.IsAuthenticated()
-	if err != nil || isAuthenticated {
-		t.Errorf("Expected to not be authenticated because we have just signed out")
-	}
-}
-
-func TestWhenErrorOccursAfterTheCallbackFromTodoistThenAnErrorIsReturned(t *testing.T) {
-	mockAPI := &mocks.MockAPI{
-		RevokeAccessTokenFunction: func(accessToken string) error {
-			return nil
-		},
-	}
-	mockRepository := &mocks.MockAuthenticationRepository{
-		AccessToken: "access-token",
-	}
-	mockServer := &mocks.MockAuthenticationServer{
-		AuthenticationResponseErrorToReturn: errors.New("test"),
-	}
-	configuration := &config.TodoistCliConfiguration{}
-
-	service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
-
-	err := service.SignIn("code")
-	if err.Error() != errorNoAuthCodeReceived {
-		t.Errorf("Expected '%s', but received '%s'", errorNoAuthCodeReceived, err.Error())
-	}
-}
-
-func TestWhenSigningInAndCodeWasNotReturnedFromTodoistThenAnErrorShouldBeReturned(t *testing.T) {
-	mockAPI := &mocks.MockAPI{
-		GetAccessTokenFunction: func(code string) (*responses.AccessToken, error) {
-			return &responses.AccessToken{AccessToken: ""}, nil
-		},
-	}
-	mockRepository := &mocks.MockAuthenticationRepository{
-		AccessToken: "access-token",
-	}
-	mockServer := &mocks.MockAuthenticationServer{
-		AuthenticationResponseToReturn: types.AuthenticationResponse{},
-	}
-	configuration := &config.TodoistCliConfiguration{}
-
-	service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
-
-	err := service.SignIn("code")
-	if err == nil {
-		t.Errorf("Expected '%s', but received nothing", errorNoAccessTokenReceived)
-	}
-}
-
-func TestWhenSigningInAndNoErrorsOccurThenSuccessfullySignedIn(t *testing.T) {
-	mockAPI := &mocks.MockAPI{
-		GetAccessTokenFunction: func(code string) (*responses.AccessToken, error) {
-			return &responses.AccessToken{AccessToken: "access-token"}, nil
-		},
-	}
-	mockRepository := &mocks.MockAuthenticationRepository{
-		AccessToken: "access-token",
-	}
-	mockServer := &mocks.MockAuthenticationServer{
-		AuthenticationResponseToReturn: types.AuthenticationResponse{},
-	}
-	configuration := &config.TodoistCliConfiguration{}
-
-	service := NewAuthenticationService(mockAPI, mockRepository, *configuration, mockServer)
-
-	err := service.SignIn("code")
-	if err != nil {
-		t.Errorf("Expected '%s', but received '%s'", errorNoAccessTokenReceived, err.Error())
-	}
 }
